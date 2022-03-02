d9485bb5e00723c12e9e7abeb3de07c7543c6bd8
dongha <ivy3.141592@gmail.com>
2022-03-02 22:41:54 +0900

    PC GUi im th

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pyautogui
-pyautogui.mouseInfo()
+b = pyautogui.locateOnScreen('pngegg.png')
+print(b)
 
 #첫 수업 시작 전 GuI 실험
+#pip install opencv-python
+#아.. cpu 메모리 너무 낮을 걸 사서 그런지 코딩하기 쉽지않.. 굉장히
